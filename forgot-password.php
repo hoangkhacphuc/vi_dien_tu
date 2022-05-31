dee2b7fb33f28ff45f6eaf056a0bee20688e50bc
= <=>
2022-05-31 11:08:42 +0700

    fix forgot password

--- forgot-password.php
+++ forgot-password.php
@@ -97,6 +97,14 @@
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-4 col-md-offset-4">
+                    <div class="login-footer flex-space-bw">
+                        <a href="index.php">Đăng nhập</a>
+                        <a href="register.php">Đăng ký</a>
+                    </div>
+                </div>
+            </div>
         </div>
         
     </div>
